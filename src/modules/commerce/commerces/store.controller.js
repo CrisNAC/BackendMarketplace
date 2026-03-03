9b44ca690589c73d0980582eb425b9bf3eb188ea
CrisNAC <cristian.acuna@fiuni.edu.py>
2026-03-02 21:03:16 -0300

    OM-202: Agregando obtener datos del comercio, agregando obtener todos o por filtro los productos del comercio

--- src/modules/commerce/commerces/store.controller.js
+++ src/modules/commerce/commerces/store.controller.js
@@ -1,6 +1,17 @@
 // src/modules/commerce/commerces/store.controller.js
-import { createStoreService } from "./store.service.js";
+import {
+  createStoreService,
+  getStoreByIdService,
+  getAllProductsByStoreService,
+  filterStorePriductsService
+} from "./store.service.js";
 
+/**
+ * Controlador para crear un nuevo comercio. Recibe los datos del comercio en el cuerpo de la solicitud, llama al servicio correspondiente y maneja las respuestas y errores.
+ * @param {*} req 
+ * @param {*} res 
+ * @returns 
+ */
 export const createStore = async (req, res) => {
   try {
     const store = await createStoreService(req.body);
@@ -17,3 +28,59 @@ export const createStore = async (req, res) => {
     });
   }
 };
+
+/**
+ * Controlador para obtener un comercio por su ID. Recibe el ID del comercio como parámetro de ruta, llama al servicio correspondiente y maneja las respuestas y errores.
+ * @param {*} req 
+ * @param {*} res 
+ * @returns 
+ */
+export const getStoreById = async (req, res) => {
+  try {
+    const { id_store } = req.params;
+    const store = await getStoreByIdService(id_store);
+    return res.status(200).json(store);
+  } catch (error) {
+    return res.status(error.status || 500).json({
+      message: error.message || "Error interno"
+    });
+  }
+};
+
+/**
+ * Controlador para obtener todos los productos de una tienda específica. Recibe el ID de la tienda como parámetro de ruta, llama al servicio correspondiente y maneja las respuestas y errores.
+ * @param {*} req 
+ * @param {*} res 
+ * @returns 
+ */
+export const getAllProductsByStore = async (req, res) => {
+  try {
+    const { id_store } = req.params;
+    const products = await getAllProductsByStoreService(id_store);
+    return res.status(200).json(products);
+  } catch (error) {
+    return res.status(error.status || 500).json({
+      message: error.message || "Error interno"
+    });
+  }
+};
+
+/**
+ * Controlador para filtrar productos de una tienda específica por categoría y rango de precios. Recibe el ID de la tienda como parámetro de ruta y los filtros como query parameters, llama al servicio correspondiente y maneja las respuestas y errores.
+ * @param {*} req 
+ * @param {*} res 
+ * @returns 
+ */
+export const filterStoreProducts = async (req, res) => {
+  try {
+    const { id_store } = req.params;
+    const { category, price_min, price_max } = req.query;
+    const products = await filterStorePriductsService(id_store, { category, price_min, price_max });
+    return res.status(200).json(products);
+  } catch (error) {
+    return res.status(error.status || 500).json({
+      message: error.message || "Error interno"
+    });
+  }
+};
+
