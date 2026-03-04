e26670e5ba2d0326f09480d4164a7cd4a0c56ba9
Lianyang1234 <Lian.Yang@fiuni.edu.py>
2026-03-03 22:35:19 -0300

    Backend de producto, categoriaproducto, tagsproducto etc.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import cors from "cors";
 import storeRoutes from "./modules/commerce/commerces/store.routes.js";
 import productRoutes from "./modules/commerce/products/product.routes.js";
 import productCategoryRoutes from "./modules/commerce/product-categories/product-category.routes.js";
@@ -13,6 +14,10 @@ const PORT = process.env.PORT || 3000;
 //Para debug en consola de las peticiones
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+  credentials: true
+}));
 
 app.use("/api/commerces",storeRoutes);
 app.use("/products", productRoutes);
