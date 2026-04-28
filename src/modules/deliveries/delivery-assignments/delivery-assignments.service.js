f263823d11346000cbaca6fb4e93094c8f8b106e
Benjakr04 <benjaminkravchuk04@gmail.com>
2026-04-28 16:04:30 -0300

    OM-489: he creado delivery.controller.js

---
+++ src/modules/deliveries/delivery-assignments/delivery-assignments.service.js
@@ -0,0 +1,14 @@
+import { prisma } from '../../../lib/prisma.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+
+// aceptar asignación
+export const acceptAssignmentService = async (assignmentId) => {
+    
+};
+
+// rechazar asignación
+export const rejectAssignmentService = async (assignmentId) => {
+  // Tu lógica aquí
+};
---
+++ src/modules/deliveries/delivery/delivery.controller.js
@@ -0,0 +1,61 @@
+//delivery.controller.js
+import {registerDeliverySchema, createDeliverySchema, loginDeliverySchema, updateDeliveryStatusSchema} from './delivery.validation';
+import { registerDeliveryService, createDeliveryService, loginDeliveryService, updateDeliveryStatusService, getPendingAssignmentsService } from './delivery.service';
+
+//registrar delivery
+export const registerDelivery = async (req, res) => {
+    try{
+        //validar con zod
+        const validData = registerDeliverySchema.parse(req.body);
+        //llamar al service
+        const result = await registerDeliveryService(validData);
+        // Retornar respuesta
+        res.status(201).json(result);
+    }catch (error){
+        // Manejar errores
+        res.status(error.status || 500).json({ error: error.message });
+    }
+}
+// login delivery
+export const loginDelivery = async (req, res) => {
+  try {
+    const validData = loginDeliverySchema.parse(req.body);
+    const result = await loginDeliveryService(validData.email, validData.password);
+    res.json(result);
+  } catch (error) {
+    res.status(error.status || 500).json({ error: error.message });
+  }
+};
+//crear delivery
+export const createDelivery = async (req, res) => {
+  try {
+    const validData = createDeliverySchema.parse(req.body);
+    const result = await createDeliveryService(validData);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(error.status || 500).json({ error: error.message });
+  }
+};
+
+//actualizar status
+export const updateDeliveryStatus = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const validData = updateDeliveryStatusSchema.parse(req.body);
+    const result = await updateDeliveryStatusService(parseInt(id), validData.delivery_status);
+    res.json(result);
+  } catch (error) {
+    res.status(error.status || 500).json({ error: error.message });
+  }
+};
+
+// obtener asignaciones pendientes
+export const getPendingAssignments = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await getPendingAssignmentsService(parseInt(id));
+    res.json(result);
+  } catch (error) {
+    res.status(error.status || 500).json({ error: error.message });
+  }
+};
