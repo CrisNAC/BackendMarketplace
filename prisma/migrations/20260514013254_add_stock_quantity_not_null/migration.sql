edf6a7e6f9b1fc40fd3219d0dea2e452d15b4328
CrisNAC <cristian.acuna@fiuni.edu.py>
2026-05-13 22:54:34 -0300

    cristian-OM-514: Update migration script: set existing NULL quantities to 0 before making the column required

--- prisma/migrations/20260514013254_add_stock_quantity_not_null/migration.sql
+++ prisma/migrations/20260514013254_add_stock_quantity_not_null/migration.sql
@@ -4,6 +4,10 @@
   - Made the column `quantity` on table `Products` required. This step will fail if there are existing NULL values in that column.
 
 */
+
+-- Primero, actualizar los valores NULL existentes a 0
+UPDATE "Products" SET "quantity" = 0 WHERE "quantity" IS NULL;
+
 -- AlterTable
 ALTER TABLE "Products" ALTER COLUMN "quantity" SET NOT NULL,
 ALTER COLUMN "quantity" SET DEFAULT 0;
