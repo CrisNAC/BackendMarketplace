9e89356e03030e29050c6a1d5b17462f6dbc9040
J-Kanami-PS <jazminpavon@fiuni.edu.py>
2026-05-30 22:44:49 -0300

    OM-560-SEC-A02: modificaciones server.js, README.md y .env.example.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import { validateEnv } from "./config/env.config.js";
 
 dotenv.config();
+
+validateEnv();
 
 const PORT = process.env.PORT || 3000;
 
